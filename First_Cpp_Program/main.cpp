7796e6ca39c7864f7b9b592d5ce79a69f38af8e2
ChamiLamelas <chami.lamelas@gmail.com>
2022-06-25 16:26:28 -0400

    added note on namespaces

--- First_Cpp_Program/main.cpp
+++ First_Cpp_Program/main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
+/*
+In the past, I thought you should have using namespace std; at the top but people seem inclined to keep
+the namespace specfication before members to avoid confusion, see here:
+https://stackoverflow.com/questions/1452721/why-is-using-namespace-std-considered-bad-practice?page=1&tab=scoredesc#tab-top 
+*/
+
 int main() {
     std::cout << "Hello World!" << std::endl;
     return 0;
